14920bab90f9102dea9a297ea6c1e3bf46676f74
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-15 00:02:43 +0200

    Clicking on liked project redirect so who view

--- src/resources/users/show.tsx
+++ src/resources/users/show.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
 import { FC } from "react";
-import { Show, SimpleShowLayout, TextField, ReferenceArrayField, SingleFieldList, ChipField, ShowProps } from 'react-admin';
+import {
+  Show,
+  SimpleShowLayout,
+  TextField,
+  ReferenceArrayField,
+  SingleFieldList,
+  ChipField,
+  ShowProps,
+} from "react-admin";
 
 const UserShow: FC<ShowProps> = (props) => {
-    return (
-        <Show {...props} title=" ">
-            <SimpleShowLayout>
-                <TextField source="password" />
-                <TextField source="password_salt" />
-                <ReferenceArrayField reference="projects" source="liked_projects" sortable={false}>
-                    <SingleFieldList>
-                        <ChipField source="title" />
-                    </SingleFieldList>
-                </ReferenceArrayField>
-            </SimpleShowLayout>
-        </Show>
-    );
+  return (
+    <Show {...props} title=" ">
+      <SimpleShowLayout>
+        <TextField source="password" />
+        <TextField source="password_salt" />
+        <ReferenceArrayField reference="projects" source="liked_projects" sortable={false}>
+          <SingleFieldList linkType="show">
+            <ChipField source="title" />
+          </SingleFieldList>
+        </ReferenceArrayField>
+      </SimpleShowLayout>
+    </Show>
+  );
 };
 
 export default UserShow;
