5717a839bceca85fc1c9d34afe8f6b6909308da2
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-15 00:26:31 +0200

    Fixed formatting

--- src/resources/projects/show.tsx
+++ src/resources/projects/show.tsx
@@ -1,42 +1,55 @@
 import { TitleProps } from "ra-ui-materialui/lib/layout/Title";
 import * as React from "react";
 import { FC } from "react";
-import { Show, SimpleShowLayout, TextField, ReferenceArrayField, SingleFieldList, ShowProps, BooleanField, NumberField, ImageField, ReferenceField, DeleteButton, ShowActionsProps, TopToolbar } from 'react-admin';
+import {
+  Show,
+  SimpleShowLayout,
+  TextField,
+  ReferenceArrayField,
+  SingleFieldList,
+  ShowProps,
+  BooleanField,
+  NumberField,
+  ImageField,
+  ReferenceField,
+  DeleteButton,
+  ShowActionsProps,
+  TopToolbar,
+} from "react-admin";
 
 const ProjectTitle: FC<TitleProps> = ({ record }) => {
-    return <span>Project {record ? `"${record.title}"` : ''}</span>;
+  return <span>Project {record ? `"${record.title}"` : ""}</span>;
 };
 
-
 const ProjectActions: FC<ShowActionsProps> = ({ basePath, data, resource }) => (
-    <TopToolbar>
-        <DeleteButton basePath={basePath} record={data} resource={resource} />
-    </TopToolbar>
+  <TopToolbar>
+    <DeleteButton basePath={basePath} record={data} resource={resource} />
+  </TopToolbar>
 );
 
 const ProjectShow: FC<ShowProps> = (props) => {
-    return (
-        <Show {...props} title={<ProjectTitle />} actions={<ProjectActions />}>
-            <SimpleShowLayout>
-                <ImageField label="Cover picture" source="cover_picture_url" title="title" />
-                <TextField source="title" />
-                <TextField source="short_description" />
-                <TextField source="description" />
-                <ReferenceField label="Owner" source="owner" reference="users" sortable={false}>
-                    <TextField source="username" />
-                </ReferenceField>
-                <ReferenceArrayField reference="users" source="members">
-                    <SingleFieldList>
-                        <TextField source="username" />
-                    </SingleFieldList>
-                </ReferenceArrayField>
-                <TextField source="status" />
-                <BooleanField source="published" />
-                <NumberField source="likes" />
-                <TextField source="id" />
-            </SimpleShowLayout>
-        </Show>
-    );
+  return (
+    <Show {...props} title={<ProjectTitle />} actions={<ProjectActions />}>
+      <SimpleShowLayout>
+        <ImageField label="Cover picture" source="cover_picture_url" title="title" />
+        <TextField source="title" />
+        <TextField source="short_description" />
+        <TextField source="description" />
+        <ReferenceField label="Owner" source="owner" reference="users" sortable={false}>
+          <TextField source="username" />
+        </ReferenceField>
+        <ReferenceArrayField reference="users" source="members">
+          <SingleFieldList>
+            <TextField source="username" />
+          </SingleFieldList>
+        </ReferenceArrayField>
+        <TextField source="status" />
+        <BooleanField source="published" />
+        <NumberField source="likes" />
+        <TextField source="id" />
+      </SimpleShowLayout>
+    </Show>
+  );
 };
 
 export default ProjectShow;
