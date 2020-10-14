92aac390410f5f9f23e96cc51f804f85c72590f3
Pierre Demessence <pierre.demessence@gmail.com>
2020-10-15 00:05:15 +0200

    Removed password and password salt

--- src/resources/users/show.tsx
+++ src/resources/users/show.tsx
@@ -1,21 +1,11 @@
 import * as React from "react";
 import { FC } from "react";
-import {
-  Show,
-  SimpleShowLayout,
-  TextField,
-  ReferenceArrayField,
-  SingleFieldList,
-  ChipField,
-  ShowProps,
-} from "react-admin";
+import { Show, SimpleShowLayout, ReferenceArrayField, SingleFieldList, ChipField, ShowProps } from "react-admin";
 
 const UserShow: FC<ShowProps> = (props) => {
   return (
     <Show {...props} title=" ">
       <SimpleShowLayout>
-        <TextField source="password" />
-        <TextField source="password_salt" />
         <ReferenceArrayField reference="projects" source="liked_projects" sortable={false}>
           <SingleFieldList linkType="show">
             <ChipField source="title" />
